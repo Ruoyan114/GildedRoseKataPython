effae6ffb91bc098bc953ef84b595ae27c82f53b
ruoyanj01 <jiang.ruoy@northeastern.edu>
2025-02-05 22:39:33 -0800

    added 4 tests

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,11 +5,50 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    # example of test that checks for logical errors
+    def test_sulfuras_should_not_decrease_quality(self):
+        items = [Item("Sulfuras", 5, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        sulfuras_item = items[0]
+        self.assertEquals(80, sulfuras_item.quality)
+        self.assertEquals(4, sulfuras_item.sell_in)
+        self.assertEquals("Sulfuras", sulfuras_item.name)
+
+    # example of test that checks for syntax errors
+    def test_gilded_rose_list_all_items(self):
+        items = [Item("Sulfuras", 5, 80)]
+        gilded_rose = GildedRose(items)
+        all_items = gilded_rose.get_items()
+        self.assertEqual(["Sulfuras"], all_items)
+
+
+    # logical error test 1 - The Quality of an item is never more than 50
+    def test_aged_brie_quality_never_more_than_50(self):
+        items = [Item("Aged Brie", 2, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(51, items[0].quality)
+
+
+    # logical error test 2 - conjured items should degrade in quality as twice as fast
+    def test_conjured_items_degrade_twice_as_fast(self):
+        items = [Item("Conjured Mana Cake", 4, 16)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(14, items[0].quality)
+
+    # logical error test 3 - the quality of an item should never be negative
+    def test_quality_never_negative(self):
+        items = [Item("Normal Item", 3, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(-1, items[0].quality)
+
+    # Syntax error test
+    def test_gilded_rose_incorrect_instantiation(self):
+        with self.assertRaise(TypeError):
+            gilded_rose = GildedRose()
 
 
 if __name__ == '__main__':
